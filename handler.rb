bcf1f61fbfa3af8cafc7edd82a6b229cb363ce8b
watase.atsushi <beechan920@yahoo.co.jp>
2023-02-06 08:13:50 +0900

    APIのログ出力機能追加

--- handler.rb
+++ handler.rb
@@ -1,7 +1,12 @@
-require 'json'
 require "aws-sdk-dynamodb"
+require 'json'
+require 'logger'
+
 
 def hello(event:, context:)
+  logger = Logger.new('./development.log') if event["headers"]["host"] == "localhost:3000" # ローカルでの開発時のログ出力用
+  logger.info('## EVENT hello')
+  logger.info(event)
   {
     statusCode: 200,
     body: {
@@ -12,6 +17,9 @@ def hello(event:, context:)
 end
 
 def dynamodb_access(event:, context:)
+  logger = Logger.new('./development.log') if event["headers"]["host"] == "localhost:3000" # ローカル環境でのログ出力用
+  logger.info('## EVENT dynamodb_access')
+  logger.info(event)
   Aws.config.update({
     region: 'ap-northeast-1',
     endpoint:'http://host.docker.internal:8000'
